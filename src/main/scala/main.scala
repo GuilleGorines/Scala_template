8f71a1e37f1506a0fabbd3894e44fa2b3a4efe38
GuilleGorines <60110848+GuilleGorines@users.noreply.github.com>
2026-02-14 11:11:50 +0100

    primer on main function

---
+++ src/main/scala/main.scala
@@ -0,0 +1,12 @@
+// import 
+
+object Geeks { 
+
+    def main(args: Array[String]) 
+    { 
+        val environment: String = args._0
+        val file: String = args._1
+
+        println(environment) 
+    } 
+}
